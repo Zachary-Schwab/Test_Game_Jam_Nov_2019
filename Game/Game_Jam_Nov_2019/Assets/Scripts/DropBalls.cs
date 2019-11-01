3f2152c0d6f76b31d7046a80f02639f2cd73e89f
Zachary_Schwab <zachary.schwab@mymail.champlain.edu>
2019-11-01 19:55:14 -0400

    ball dropping

---
+++ Game/Game_Jam_Nov_2019/Assets/Scripts/DropBalls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropBalls : MonoBehaviour
+{
+    public int ballsToDrop = 200;
+    public GameObject ball_model;
+    public float spawnHeight = 5f;
+    public Vector4 dropZoneDimensions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < ballsToDrop; i++)
+        {
+            float xSpawnPos = Random.Range(dropZoneDimensions.x, dropZoneDimensions.y);
+            float zSpawnPos = Random.Range(dropZoneDimensions.z, dropZoneDimensions.w);
+            Instantiate(ball_model, new Vector3(xSpawnPos, spawnHeight, zSpawnPos), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
