127f30acb5fa517490c11078013fc82c72d04a29
Dante Xystus <dante.xystus@mymail.champlain.edu>
2019-11-02 11:32:28 -0400

    Adding new files

---
+++ Game/Game_Jam_Nov_2019/Assets/Scripts/EMPManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EMPManager : MonoBehaviour
+{
+    public float spawningCooldown;
+    float currentCooldown;
+    GameObject[] positions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Add all spawning positions into the array
+        currentCooldown = spawningCooldown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown();
+    }
+
+    void countdown()
+    {
+        currentCooldown -= Time.deltaTime;
+
+        if(currentCooldown <= 0f)
+        {
+            spawnEMP();
+            currentCooldown = spawningCooldown;
+        }
+
+    }
+
+    void spawnEMP()
+    {
+        //Gen random position
+        //EMP = Instantiate(Gameobject, rand_position, rotation)
+        //EMP will update its own cooldown
+    }
+
+}
---
+++ Game/Game_Jam_Nov_2019/Assets/Scripts/EMPScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EMPScript : MonoBehaviour
+{
+
+    public GameObject player1;
+    public GameObject player2;
+
+    public float cooldown;
+    float currentCooldown;
+
+    public float empStunTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentCooldown = cooldown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown();
+    }
+
+    void countdown()
+    {
+        cooldown -= Time.deltaTime;
+
+        if(cooldown <= 0f)
+        {
+            Destroy(gameObject);
+
+            cooldown = currentCooldown;
+        }
+    }
+
+    private void OnCollisionEnter (Collision col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            if(col.gameObject.name == player1.name)
+            {
+                player2.GetComponent<AimingScript>().EMP(empStunTime);
+            }
+            else if(col.gameObject.name == player2.name)
+            {
+                player1.GetComponent<AimingScript>().EMP(empStunTime);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
