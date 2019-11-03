30dbe12f7b31dd1f7483a47dedd58aedc4e95af7
Dante Xystus <dante.xystus@mymail.champlain.edu>
2019-11-03 15:24:25 -0500

    Title Screen

---
+++ Game/Game_Jam_Nov_2019/Assets/ButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ButtonScript : MonoBehaviour
+{
+
+
+    public void swapToHelp()
+    {
+        SceneManager.LoadScene("How_To_Play");
+    }
+
+    public void swapToGame()
+    {
+        SceneManager.LoadScene("First_Single_Player_Test");
+    }
+
+}
